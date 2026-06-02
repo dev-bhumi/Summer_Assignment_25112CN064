2456fa33d81d6b564e0cf167b2b5be3efd0ef7ef
dev-bhumi <bhumisarojsingh100@gmail.com>
2026-06-02 12:21:08 +0530

    Update countdigits.c

--- Day-1/countdigits.c
+++ Day-1/countdigits.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main(){
     int n;
@@ -11,18 +10,4 @@ int main(){
     }
     printf("total no. of digits:%d",count);
     return 0;
-=======
-#include<stdio.h>
-int main(){
-    int n;
-    int count = 0;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    while(n!=0){
-    n=n/10;
-    count++;
-    }
-    printf("total no. of digits:%d",count);
-    return 0;
->>>>>>> f67b40d0012b619a44f80db9849cf9ad89b02446
 }
