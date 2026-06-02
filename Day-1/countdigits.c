120bfe3afadf801914290cc152ee0b3dff2c64ba
dev-bhumi <bhumisarojsingh100@gmail.com>
2026-06-02 12:07:28 +0530

    Add files via upload

---
+++ Day-1/countdigits.c
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+#include<stdio.h>
+int main(){
+    int n;
+    int count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while(n!=0){
+    n=n/10;
+    count++;
+    }
+    printf("total no. of digits:%d",count);
+    return 0;
+=======
+#include<stdio.h>
+int main(){
+    int n;
+    int count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while(n!=0){
+    n=n/10;
+    count++;
+    }
+    printf("total no. of digits:%d",count);
+    return 0;
+>>>>>>> f67b40d0012b619a44f80db9849cf9ad89b02446
+}
