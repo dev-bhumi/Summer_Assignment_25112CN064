774c29a296c0824e5ae428e8fb28280758306241
dev-bhumi <bhumisarojsingh100@gmail.com>
2026-06-02 12:08:13 +0530

    Add files via upload

---
+++ Day-1/sumofnumbers.c
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+#include<stdio.h>
+int main() {
+    int n;
+    int last_digit = 0;
+    int sum = 0;
+    printf ("enter first n natural number : ");
+    scanf("%d", &n);
+    while(n!=0){
+        last_digit=n%10;
+        sum = sum+last_digit;
+        n=n/10;
+    }
+    printf("sum of first n natural no.=%d", sum);
+    return 0;
+=======
+#include<stdio.h>
+int main() {
+    int n;
+    int last_digit = 0;
+    int sum = 0;
+    printf ("enter first n natural number : ");
+    scanf("%d", &n);
+    while(n!=0){
+        last_digit=n%10;
+        sum = sum+last_digit;
+        n=n/10;
+    }
+    printf("sum of first n natural no.=%d", sum);
+    return 0;
+>>>>>>> f67b40d0012b619a44f80db9849cf9ad89b02446
+}
