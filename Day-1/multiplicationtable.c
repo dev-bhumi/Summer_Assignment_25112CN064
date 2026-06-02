476164822bbd2eb76d8b81696ffcf50c835f5a8f
dev-bhumi <bhumisarojsingh100@gmail.com>
2026-06-02 12:08:45 +0530

    Add files via upload

---
+++ Day-1/multiplicationtable.c
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    for(int i=n;i<=n*10;i=i+n){
+     printf("%d\n", i);
+    }
+    return 0;
+=======
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    for(int i=n;i<=n*10;i=i+n){
+     printf("%d\n", i);
+    }
+    return 0;
+>>>>>>> f67b40d0012b619a44f80db9849cf9ad89b02446
+}
